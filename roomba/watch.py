f8ac511c5443f9b7e36a1dbb4f5547addc32361b
benediktstroebl <stroebl@princeton.edu>
2025-06-26 16:33:44 -0700

    add automatic framestacking to watch

--- roomba/watch.py
+++ roomba/watch.py
@@ -6,16 +6,7 @@ from roomba.environments.sumo_v1 import Sumo
 from roomba.environments.wrappers import FrameStackWrapper
 from roomba.models.mlp import MlpContinuousActorCritic
 from roomba.utils import get_device
-
-
-
-def load_checkpoint(
-    checkpoint_path: str,
-    device: str = "cpu",
-):  
-    agent = torch.load(checkpoint_path, map_location=device, weights_only=False)
-    agent.eval()
-    return agent
+from roomba.server.helper import get_framestack_size, load_checkpoint
 
 
 def visualize_match(env, agent1, agent2, device, n_episodes=5):
@@ -81,20 +72,20 @@ def main(
     ckpt1: str,
     ckpt2: str = None,
     episodes: int = 5,
-    frame_stack: int = 1,
     env_mode: str = "uwb",
 ):
     device = get_device()
     env = Sumo(mode=env_mode, train=False, render_mode="human")
+    
+    agent1 = load_checkpoint(ckpt1)
+    
+    # determine framestack size
+    frame_stack = get_framestack_size(agent1, env)
+    print(f"Framestack size: {frame_stack}")
+        
     if frame_stack > 1:
         env = FrameStackWrapper(env, k=frame_stack)
 
-    obs_dim = env.observation_spaces["maximus"].shape[
-        0
-    ]  # Use actual shape from environment
-    action_dim = env.action_spaces["maximus"].shape[0]
-
-    agent1 = load_checkpoint(ckpt1)
     if ckpt2 is None:
         agent2 = deepcopy(agent1)
     else:
